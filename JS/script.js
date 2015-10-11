4fdda564d8c9e0415732b5bf8b48de3bfc37579a
ebrillhart <emma.brillhart@gmail.com>
2015-10-11 12:10:39 -0700

    functional buttons added to switch between screens

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,53 @@
 $(document).ready(function() {
 
+// Buttons switching between screens
+// Homescreen to How-to
+$("#howtobutton").on("click", function(){
+	$("#homescreen").hide();
+	$("#howto").show();
+});
+
+// Homescreen to game
+$("#startgame").on("click", function(){
+	$("#homescreen").hide();
+	$("#game").show();
+});
+
+// Homescreen to info
+$("#about").on("click", function() {
+	$("#homescreen").hide();
+	$("#gameinfo").show();
+});
+
+// How-to to Home
+$("#howtohome").on("click", function() {
+	$("#howto").hide();
+	$("#homescreen").show();
+});
+
+// How-to to game
+$("#howtostart").on("click", function(){
+	$("#howto").hide();
+	$("#game").show();
+});
+
+// Win screen to game
+$("#winstart").on("click", function(){
+	$("#wininfo").hide();
+	$("#game").show();
+});
+
+// Win screen to home
+$("#winhome").on("click", function(){
+	$("#wininfo").hide();
+	$("#homescreen").show();
+});
+
+// Info to home
+$("#infohome").on("click", function(){
+	$("#gameinfo").hide();
+	$("#homescreen").show();
+});
+
 
 });
